d7628031be40f78ee731187c882e75c0b4f0121e
Luiz Gustavo Chaves dias <111753842+dias0001@users.noreply.github.com>
2022-11-10 12:02:59 -0300

    update
    Criadas as tabelas  no migrations.

---
+++ database/migrations/2022_11_10_140649_create_viagems_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('viagems', function (Blueprint $table) {
+            $table->id();
+            $table->string('saindo');
+            $table->string('indo');
+            $table->date('data_viagem');
+            $table->string('vagas_dispo');
+            $table->integer('id_user_motorista');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('viagems');
+    }
+};
